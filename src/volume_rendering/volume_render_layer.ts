1a47aa97c7d890c342b55d58de4473930e6e2609
Sean Martin <martins7@tcd.ie>
2024-07-30 11:48:00 +0200

    fix: remove CDF fixing for 0s

--- src/volume_rendering/volume_render_layer.ts
+++ src/volume_rendering/volume_render_layer.ts
@@ -31,7 +31,7 @@ import {
   renderScaleHistogramBinSize,
 } from "#src/render_scale_statistics.js";
 import { SharedWatchableValue } from "#src/shared_watchable_value.js";
-import { DataType, getNormalizedChunkLayout } from "#src/sliceview/base.js";
+import { getNormalizedChunkLayout } from "#src/sliceview/base.js";
 import type { FrontendTransformedSource } from "#src/sliceview/frontend.js";
 import {
   getVolumetricTransformedSources,
@@ -597,17 +597,8 @@ ${getShaderType(dataType)} getDataValue() { return getDataValue(0); }
           let histogramFetchCode = `
   float x;
   ${getShaderType(dataType)} inputDataValue;
-  bool should_discard = false;
   switch (uHistogramIndex) {`;
           for (let i = 0; i < numHistograms; ++i) {
-            let discardCode = `should_discard = false;`;
-            if (
-              dataType === DataType.UINT8 ||
-              dataType === DataType.UINT16 ||
-              dataType === DataType.UINT32
-            ) {
-              discardCode = `should_discard = (toNormalized(inputDataValue) == 0.0);`;
-            }
             const { channel } = dataHistogramChannelSpecifications[i];
             const getDataValueExpr = `getDataValue(${channel.join(",")})`;
             const invlerpName = `invlerpForHistogram${i}`;
@@ -630,7 +621,6 @@ ${getShaderType(dataType)} getHistogramDataValue${i}() {
             histogramFetchCode += `
   case ${i}:
     inputDataValue = getHistogramDataValue${i}();
-    ${discardCode}
     x = getHistogramValue${i}(inputDataValue);
     break;`;
           }
@@ -645,15 +635,10 @@ ${getShaderType(dataType)} getHistogramDataValue${i}() {
     simpleFloatHash(vec2(aInput1 + float(gl_VertexID) + 20.0, 15.0 + float(gl_InstanceID))));
   chunkSamplePosition = rand3val * (uChunkDataSize - 1.0);
 ${histogramFetchCode}
-  if (should_discard) {
-    gl_Position = vec4(2.0, 2.0, 2.0, 1.0);
-  }
-  else {
-    if (x < 0.0) x = 0.0;
-    else if (x > 1.0) x = 1.0;
-    else x = (1.0 + x * 253.0) / 255.0;
-    gl_Position = vec4(2.0 * (x * 255.0 + 0.5) / 256.0 - 1.0, 0.0, 0.0, 1.0);
-  }
+  if (x < 0.0) x = 0.0;
+  else if (x > 1.0) x = 1.0;
+  else x = (1.0 + x * 253.0) / 255.0;
+  gl_Position = vec4(2.0 * (x * 255.0 + 0.5) / 256.0 - 1.0, 0.0, 0.0, 1.0);
   gl_PointSize = 1.0;`);
           builder.setFragmentMain(`
 outputValue = vec4(1.0, 1.0, 1.0, 1.0);
