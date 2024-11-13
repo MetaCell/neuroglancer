0aa7e1da90c144b69ca686be85a96d44687c293e
afonso pinto <afonsobspinto@gmail.com>
2024-11-13 16:58:56 +0000

    Revert "NGLASS-215 feat: Dispatch message on screenhsot creation"
    This reverts commit c778bec17b5aa5737dd3a3904c6b7f93af3afe20.

--- src/services/stateService.ts
+++ src/services/stateService.ts
@@ -1,56 +1,13 @@
 import { getCachedJson } from "#src/util/trackable.js";
 
-export const STATE_UPDATE = "STATE_UPDATE" as const;
-export const NEW_FIGURE = "NEW_FIGURE" as const;
-export const OTHER = "OTHER" as const;
+type MessageType = "STATE_UPDATE" | "OTHER";
 
-export type MessageType = typeof STATE_UPDATE | typeof NEW_FIGURE | typeof OTHER;
-
-export interface ViewerState {
-    dimensions: Record<string, [number, string]>;
-    position: [number, number, number];
-    crossSectionScale: number;
-    projectionScale: number;
-    layers: Layer[];
-    selectedLayer: SelectedLayer;
-    layout: string;
-  }
-  
-  export interface Layer {
-    type: string;
-    source: string;
-    tab: string;
-    name: string;
-  }
-  
-  export interface SelectedLayer {
-    visible: boolean;
-    layer: string;
-  }
-  
-  export interface Screenshot {
-    id: string;
-    image: string;  // base64 encoded image
-    imageType: string;
-    width: number;
-    height: number;
-  }
-  
-  export interface NewFigurePayload {
-    viewerState: ViewerState;
-    selectedValues: Record<string, any>;  
-    screenshot: Screenshot;
-  }
-    
-  export interface SessionUpdatePayload {
+export interface SessionUpdatePayload {
     url: string;
     state: any; // Adjust as needed for more specific typing
 }
 
-export type DispatchablePayload = SessionUpdatePayload | NewFigurePayload;
-
-
-export function dispatchMessage(type: MessageType, payload: DispatchablePayload): void {
+export function dispatchMessage(type: MessageType, payload: SessionUpdatePayload): void {
     window.postMessage({ type, payload }, "*");
 }
 
