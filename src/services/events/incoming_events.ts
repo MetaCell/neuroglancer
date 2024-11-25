2385f5419398f12c68ba5631a3e043f4f3ef419b
afonso pinto <afonsobspinto@gmail.com>
2024-11-25 14:38:59 +0000

    NGLASS-302 feat: Add incoming events handler

---
+++ src/services/events/incoming_events.ts
@@ -0,0 +1,45 @@
+import { RefCounted } from "#src/util/disposable.js";
+
+export type MessagePayload = {
+  type: string;
+  payload: Record<string, any>;
+};
+
+export const CREATE_FIGURE = "CREATE_FIGURE" as const;
+
+
+export class IncomingEventsHandler extends RefCounted {
+  constructor(public viewer: any) {
+    super();
+    this.initialize();
+  }
+
+  private initialize() {
+    this.registerEventListener(window, 'message', this.handlePostMessage.bind(this));
+  }
+
+
+  private handlePostMessage(event: MessageEvent) {
+    const { data } = event;
+
+    if (!data || typeof data.type !== 'string') {
+      console.warn('Invalid message received', event);
+      return;
+    }
+
+    const message: MessagePayload = data;
+    this.handleMessageType(message);
+  }
+
+  private handleMessageType(message: MessagePayload) {
+    switch (message.type) {
+      case CREATE_FIGURE:
+        this.createFigure();
+        break;
+    }
+  }
+
+  private createFigure() {
+    this.viewer.showScreenshotDialog()
+  }
+}
