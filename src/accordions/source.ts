ab02099a23256ffea25896f94f5d04ee0ae90b62
Diogo Correia <diogo@metacell.us>
2025-01-30 11:45:01 +0000

    NGLASS-525 Support multiple data sources accordion items

--- src/accordions/source.ts
+++ src/accordions/source.ts
@@ -1,42 +1,49 @@
 import { buildAccordion } from "#src/accordions/accordion.js";
 
 export function builSourceTab(root: HTMLDivElement) {
-  const dataSourcesControlsContainer = root?.querySelector(
-    ".neuroglancer-layer-data-source-div",
+  // there can be multiple data sources container
+  const dataSourcesContainers = root.getElementsByClassName(
+    "neuroglancer-layer-data-source",
   );
-  const dataSourcesContainer = root.querySelector(
-    ".neuroglancer-layer-data-source",
-  );
+  if (dataSourcesContainers.length === 0) {
+    return;
+  }
 
-  buildAccordion(dataSourcesContainer, [
-    {
-      title: "Data source",
-      open: true,
-      selectors: {
-        ids: ["dataSourceUrlInputElement"],
-        classNames: ["data-source-container"],
-      },
-    },
-  ]);
+  Array.from(dataSourcesContainers).forEach((container) => {
+    const dataSourcesControlsContainer = container.querySelector(
+      ".neuroglancer-layer-data-source-div",
+    );
 
-  buildAccordion(dataSourcesControlsContainer, [
-    {
-      title: "Enabled components",
-      itemsClassNames: [
-        "data-source-container",
-        "enabled-components-container",
-      ],
-      selectors: {
-        ids: ["enableDefaultSubsourcesLabel"],
-        classNames: ["neuroglancer-layer-data-source-subsource"],
+    buildAccordion(dataSourcesControlsContainer, [
+      {
+        title: "Enabled components",
+        itemsClassNames: [
+          "data-source-container",
+          "enabled-components-container",
+        ],
+        selectors: {
+          ids: ["enableDefaultSubsourcesLabel"],
+          classNames: ["neuroglancer-layer-data-source-subsource"],
+        },
       },
-    },
-    {
-      title: "Scale and translation",
-      itemsClassNames: ["data-source-container", "transform-container"],
-      selectors: {
-        ids: ["transformWidget"],
+      {
+        title: "Scale and translation",
+        itemsClassNames: ["data-source-container", "transform-container"],
+        selectors: {
+          ids: ["transformWidget"],
+        },
       },
-    },
-  ]);
+    ]);
+
+    buildAccordion(container, [
+      {
+        title: "Data source",
+        open: true,
+        selectors: {
+          ids: ["dataSourceUrlInputElement"],
+          classNames: ["data-source-container"],
+        },
+      },
+    ]);
+  });
 }
